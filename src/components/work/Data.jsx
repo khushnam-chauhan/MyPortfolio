ee569bfe8cb599bb91b0c054c0214dfa4cea2309
khushnam <chauhankhushnam@gmail.com>
2024-04-25 23:23:56 +0530

    projects added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,27 +1,51 @@
 import React from 'react'
 import Work1 from '../../assets/work1.jpg'
-import Work2 from '../../assets/work2.jpg'
-import Work3 from '../../assets/work3.jpg'
+import Work2 from '../../assets/work2.png'
+import Work3 from '../../assets/work3.png'
 export const projectsData=[
-    {
-        id:1,
-        image:Work1,
-        title:"E-commerce Website",
-        description:'This is a simple eCommerce website.',
+    // {
+    //     id:1,
+    //     image:Work1,
+    //     title:"E-commerce Website",
+    //     description:'This is a simple eCommerce website.',
+    //     category: "Basic",
         
-    },
+        
+    // },
     {
         id:2,
         image:Work2,
-        title:"E-commerce Website",
-        description:'This is a simple eCommerce website.',
-        
+        title:"Vastra - a clothing Ecom",
+        description:'This is an clothing eCommerce website.',
+        category: "Intermediate", 
+        Demo:"https://vastra-phi.vercel.app/"  
     },
     {
         id:3,
         image:Work3,
-        title:"E-commerce Website",
+        title:"JobMage - MERN stack",
         description:'This is a simple eCommerce website.',
+        category: "Advance",
+        Demo:"https://jobmage.vercel.app/"
         
     },
 ];
+
+export const projectsNav=[
+    {
+        name:'All',
+
+    },
+    {
+        name:'Basic',
+
+    },
+    {
+        name:'Intermediate',
+
+    },
+    {
+        name:'Advance',
+
+    },
+]
